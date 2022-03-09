7584a871912237cb880392977fcff3afb8a93a45
wagnercastillo <wagner.castillo@unl.edu.ec>
2022-03-09 17:35:03 -0500

    Funcionamiento de App Gifts

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GifGridtem } from "./GifGridtem";
 
 export const GifGrid = ({ categoria }) => {
+	const [count, setCount] = useState(0);
+
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		getGifs();
+	}, []);
+
 	const getGifs = async () => {
 		const url =
-			"https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=5&api_key=j8x276OP0NlJSP0opHPZ3H4hno9BfTF3";
+			"https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=j8x276OP0NlJSP0opHPZ3H4hno9BfTF3";
 		const resp = await fetch(url);
 		const { data } = await resp.json();
 
@@ -14,16 +23,18 @@ export const GifGrid = ({ categoria }) => {
 				url: img.images?.downsized_medium.url,
 			};
 		});
-		 
 
 		console.log(gifs);
+		setImages(gifs);
 	};
-
-	getGifs();
 
 	return (
 		<div>
 			<h3>{categoria}</h3>
+
+			{images.map((img) => (
+				<GifGridtem key={img.id} {...img} />
+			))}
 		</div>
 	);
 };
